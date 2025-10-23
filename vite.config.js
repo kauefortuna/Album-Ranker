3ca1371290e3d652c87cd362a6e1d37792f44e0a
kauefortuna <kauefortuna2707@gmail.com>
2025-10-23 15:29:17 -0400

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Album-Ranker/',
   plugins: [react()],
   server: {
     host: '127.0.0.1', // forces it to bind to this IP
